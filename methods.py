395565496d730f40c315a98c30620906e63e6111
OTHAVIO CARVALHO LOSOVOI <22.01545-0@maua.br>
2023-05-08 08:50:42 -0300

    Adicao de metodos

--- methods.py
+++ methods.py
@@ -5,3 +5,15 @@ def area_of_rectangle(width, height):
 def perimeter_of_rectangle(width, height):
     perimeter = 2 * (width + height)
     return perimeter
+
+def soma(num1, num2):
+    return num1 + num2
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+def multiplicacao(num1, num2):
+    return num1 * num2
+
+def divisao(num1, num2):
+    return num1 / num2
--- test_methods.py
+++ test_methods.py
@@ -21,3 +21,47 @@ def test_perimeter():
     
     # then the perimeter should be 14
     assert output == 14
+
+def teste_soma():
+    # dado os numeros 2 e 4
+    num1 = 2
+    num2 = 4
+
+    # quando calculamos a soma
+    output = methods.soma(num1, num2)
+
+    # o resultado deve ser 6
+    assert output == 6
+
+def teste_subtracao():
+    # dado os numeros 40 e 25
+    num1 = 40
+    num2 = 25
+    
+    # quando calculamos a subtracao
+    output = methods.subtracao(num1, num2)
+
+    # o resultado deve ser 15
+    assert output == 15
+
+def teste_multiplicacao():
+    # dado os numeros 5 e 9
+    num1 = 5
+    num2 = 9
+
+    # quando calculamos a subtracao
+    output = methods.multiplicacao(num1, num2)
+
+    # o resultado deve sert 45
+    assert output == 45
+
+def teste_divisao():
+    # dado os numeros 40 e 4
+    num1 = 40
+    num2 = 4
+
+    # quando calculamos a divisao
+    output = methods.divisao(num1, num2)
+
+    # o resultado deve ser 10
+    assert output == 10
